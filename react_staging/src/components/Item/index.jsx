ee97f176062a4e61be51384a17c3deef4e9a9635
August.joe <583572604@qq.com>
2021-07-13 17:29:31 +0800

    提交修改

--- react_staging/src/components/Item/index.jsx
+++ react_staging/src/components/Item/index.jsx
@@ -1,16 +1,33 @@
 import React, { Component } from 'react'
 
 export default class Item extends Component {
+    
+    state={
+        mouse:false
+    }
+
+    handleMouse = (flag)=>{
+        return ()=>{
+            this.setState({mouse:flag})
+        }
+    }
+
+    handleCheck = (id)=>{
+        return (event)=>{
+            console.log(event.target.checked)
+        }
+    }
+
     render() {
-        const {name,done} = this.props.item 
+        const {name,done,id} = this.props.item 
         return (
             <div>
-                <li>
+                <li style={{background:this.state.mouse?'#ddd':'white'}} onMouseLeave={this.handleMouse(false)} onMouseEnter={this.handleMouse(true)} >
                     <label>
-                        <input type="checkbox" defaultChecked={done}/>
+                        <input type="checkbox" defaultChecked={done} onChange={this.handleCheck(id)}/>
                         <span>{name}</span>
                     </label>
-                    <button className="btn btn-danger" style={{ display: "none" }}>删除</button>
+                    <button  className="btn btn-danger" style={{ display :this.state.mouse? "" : 'none' }}>删除</button>
                 </li>
             </div>
         )
