beef8fc953b45afce410e3cd5230173c8d6489a7
August.joe <583572604@qq.com>
2021-08-11 22:59:30 +0800

    完善github搜索项目

--- react_staging/src/components/List/List.jsx
+++ react_staging/src/components/List/List.jsx
@@ -1,42 +1,27 @@
 import React, { Component } from 'react'
 
 export default class List extends Component {
-    render() {
-        return (
-            <div>
-                <div className="row">
-                    <div className="card">
-                        <a href="https://github.com/reactjs" target="_blank"  rel="noreferrer">
-                            <img alt="head portrait" src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{ width: '100px' }} />
-                        </a>
-                        <p className="card-text">reactjs</p>
-                    </div>
-                    <div className="card">
-                        <a href="https://github.com/reactjs" target="_blank"  rel="noreferrer">
-                            <img alt="head portrait" src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{ width: '100px' }} />
-                        </a>
-                        <p className="card-text">reactjs</p>
-                    </div>
-                    <div className="card">
-                        <a href="https://github.com/reactjs" target="_blank"  rel="noreferrer">
-                            <img alt="head portrait" src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{ width: '100px' }} />
-                        </a>
-                        <p className="card-text">reactjs</p>
-                    </div>
-                    <div className="card">
-                        <a href="https://github.com/reactjs" target="_blank"  rel="noreferrer">
-                            <img alt="head portrait" src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{ width: '100px' }} />
-                        </a>
-                        <p className="card-text">reactjs</p>
-                    </div>
-                    <div className="card">
-                        <a href="https://github.com/reactjs" target="_blank"  rel="noreferrer">
-                            <img alt="head portrait" src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{ width: '100px' }} />
-                        </a>
-                        <p className="card-text">reactjs</p>
-                    </div>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    const {loading,isFirst,users,err} = this.props
+
+    return (
+      <div className="row">
+        {
+          isFirst ? <h2>欢迎使用</h2> :
+          loading ? <h2>正在加载请稍后</h2> :
+          err ? <h2> 出现错误 ： {err}</h2> :
+          users.map(item => {
+            return (
+              <div key={item.id} className="card">
+                <a href={item.html_url} target="_blank" rel="noreferrer">
+                  <img alt="head portrait" src={item.avatar_url} style={{ width: '100px' }} />
+                </a>
+                <p className="card-text">{item.login}</p>
+              </div>
+            )
+          })
+        }
+      </div>
+    )
+  }
 }
--- react_staging/src/components/search/search.jsx
+++ react_staging/src/components/search/search.jsx
@@ -6,12 +6,14 @@ export default class search extends Component {
         // 解构赋值的连续赋值 取出this中的searchinput中的value
         // 请求的ip要与访问地址的保持一致
         const {searchInput:{value : keysWord}} = this;
-        axios.get(`http://192.168.1.2:3000/api/search/users2?q=${keysWord}`).then(
+        this.props.changeState({loading:true,isFirst:false})
+        axios.get(`http://192.168.1.2:3000/api/search/users?q=${keysWord}`).then(
             response => {
                 console.log(response)
+               this.props.changeState({loading:false,users:response.data.items})
             },
             error => {
-                console.log(error)
+                this.props.changeState({err:error.message,loading:false})
             }
         )
     }
